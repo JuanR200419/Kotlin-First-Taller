cc0d1280b486e3667a5e87597c4936273164fd0c
JuanR200419 <141380315+JuanR200419@users.noreply.github.com>
2025-02-12 17:14:56 -0500

    feat
    new moduls and new controller

---
+++ CodeHere/src/company management/Models/Cargo.kt
@@ -0,0 +1,8 @@
+package company.management
+
+class Cargo(
+    val name: String,       // Nombre del cargo (Ejemplo: "Gerente", "Analista", "Operador")
+    val hierarchyLevel: Int // Nivel jerárquico (Ejemplo: 1 = CEO, 2 = Gerente, 3 = Supervisor, etc.)
+) {
+
+}
---
+++ CodeHere/src/company management/Models/Person.kt
@@ -0,0 +1,11 @@
+package `company management`
+
+open class Person(
+    val name: String,
+    val id: String,
+    val sex: String,
+    val email: String,
+    val phone: String){
+
+
+}
